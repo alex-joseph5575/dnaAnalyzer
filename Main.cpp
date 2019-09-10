b034bc9a20113434e888f8154c8bd45c80606c09
Alex <alex.joseph5575@gmail.com>
2019-09-10 23:54:20 +0100

    beginning

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 int main(int argc, char** argv)
 {
-
+  ifstream myFile;
+  string line;
+  myFile.open("TestFile.txt");
+  while(getLine(myFile, line))
+  {
+    cout << line << '\n';
+  }
+  myFile.close();
 
   return 0;
 }
